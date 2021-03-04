1dcc440fde1e243968df790d414d6bfe47e2848f
iamsubingyawali <subingyawali7@gmail.com>
2021-03-04 10:07:24 +0545

    Initial Commit

---
+++ encryptstego.py
@@ -0,0 +1,15 @@
+# Importing required libraries
+import tkinter as tk
+from ctypes import windll
+import Crypto
+
+window = tk.Tk()
+window.title("Encryptstego")
+window.geometry('800x500')
+window.resizable(False, False)
+windll.shcore.SetProcessDpiAwareness(1)
+
+photo = tk.PhotoImage(file="images/logo.png")
+window.iconphoto(True, photo)
+
+window.mainloop()
