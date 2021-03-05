84309bbef195e93421553f771654dc0480cac9ed
iamsubingyawali <subingyawali7@gmail.com>
2021-03-05 22:19:25 +0545

    Fixed multiple windows being displayed when buttons are clicked multiple times

--- src/encryptstego.py
+++ src/encryptstego.py
@@ -7,37 +7,85 @@ from tkinter import *
 from ctypes import windll
 from PIL import ImageTk, Image
 
+# Defining global variables to track if two child windows for encoding and decoding are in opened or closed state
+encode_opened = False
+decode_opened = False
 
+
+# function to open a new window for encoding functionality
 def open_encode_window():
-    encode_window = Toplevel(window)
-    # Setting title of the encode window
-    encode_window.title("Encryptstego - Encode")
-    # Setting the size of the encode window
-    encode_window.geometry('800x500')
-    # Setting the resizable property of the encode window to false
-    encode_window.resizable(False, False)
-    # Setting the encode window to be DPI aware for different screens
-    # windll is Windows OS specific, so must be commented before running on other environments
-    windll.shcore.SetProcessDpiAwareness(1)
-    # Test label
-    new_label = Label(encode_window, text="This is encode window")
-    new_label.pack()
+    # referencing global variable to reflect modification globally
+    global encode_opened
+    # checking if the window is already open
+    if not encode_opened:
+        # Initializing new child window for encoding functions
+        encode_window = Toplevel(window)
+        # Setting title of the encode window
+        encode_window.title("Encryptstego - Encode")
+        # Setting the size of the encode window
+        encode_window.geometry('800x500')
+        # Setting the resizable property of the encode window to false
+        encode_window.resizable(False, False)
+        # Setting the encode window to be DPI aware for different screens
+        # windll is Windows OS specific, so must be commented before running on other environments
+        windll.shcore.SetProcessDpiAwareness(1)
+        # Test label
+        new_label = Label(encode_window, text="This is encode window")
+        new_label.pack()
+        # setting the value of global tracking variable to true
+        encode_opened = True
+        # assigning a handler to define the actions when the window is tried to close
+        encode_window.protocol("WM_DELETE_WINDOW", lambda: close_encode_window(encode_window))
 
 
+# function to open a new window for decoding functionality
 def open_decode_window():
-    decode_window = Toplevel(window)
-    # Setting title of the decode window
-    decode_window.title("Encryptstego - Encode")
-    # Setting the size of the decode window
-    decode_window.geometry('800x500')
-    # Setting the resizable property of the decode window to false
-    decode_window.resizable(False, False)
-    # Setting the decode window to be DPI aware for different screens
-    # windll is Windows OS specific, so must be commented before running on other environments
-    windll.shcore.SetProcessDpiAwareness(1)
-    # Test label
-    new_label = Label(decode_window, text="This is decode window")
-    new_label.pack()
+    # referencing global variable to reflect modification globally
+    global decode_opened
+    # checking if the window is already open
+    if not decode_opened:
+        # Initializing new child window for decoding functions
+        decode_window = Toplevel(window)
+        # Setting title of the decode window
+        decode_window.title("Encryptstego - Encode")
+        # Setting the size of the decode window
+        decode_window.geometry('800x500')
+        # Setting the resizable property of the decode window to false
+        decode_window.resizable(False, False)
+        # Setting the decode window to be DPI aware for different screens
+        # windll is Windows OS specific, so must be commented before running on other environments
+        windll.shcore.SetProcessDpiAwareness(1)
+        # Test label
+        new_label = Label(decode_window, text="This is decode window")
+        new_label.pack()
+        # setting the value of global tracking variable to true
+        decode_opened = True
+        # assigning a handler to define the actions when the window is tried to close
+        decode_window.protocol("WM_DELETE_WINDOW", lambda: close_decode_window(decode_window))
+
+
+# Function to define actions to be performed when encoding window is tried to closed
+def close_encode_window(encode_window):
+    # referencing global variable to reflect modification globally
+    global encode_opened
+    # Destroying the passed child encoding window
+    encode_window.destroy()
+    # Setting the value of global tracking variable to false to denote that window is closed
+    encode_opened = False
+    # Test print statement
+    print("Encode Window Closed")
+
+
+# Function to define actions to be performed when decoding window is tried to closed
+def close_decode_window(decode_window):
+    # referencing global variable to reflect modification globally
+    global decode_opened
+    # Destroying the passed child encoding window
+    decode_window.destroy()
+    # Setting the value of global tracking variable to false to denote that window is closed
+    decode_opened = False
+    # Test print statement
+    print("Decode Window Closed")
 
 
 # Initializing tkinter window
