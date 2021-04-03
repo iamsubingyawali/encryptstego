77406ab0c18d5472af89c7d8ee4168d8bb9b0263
iamsubingyawali <subingyawali7@gmail.com>
2021-04-03 20:25:35 +0545

    1. Added functionality to browse images from device and display them on the window
    2. Finalized text fields and image display frames design on both windows
    
    3. Made child windows to stay on top of parent window

--- src/encryptstego.py
+++ src/encryptstego.py
@@ -31,12 +31,51 @@ def open_encode_window():
         encode_window.geometry('800x500')
         # Setting the resizable property of the encode window to false
         encode_window.resizable(False, False)
+        # Setting the window as child of parent so that it appears at front
+        encode_window.transient(window)
         # Setting the encode window to be DPI aware for different screens
         # windll is Windows OS specific, so must be commented before running on other environments
         windll.shcore.SetProcessDpiAwareness(1)
-        # Test label
-        new_label = Label(encode_window, text="This is encode window")
-        new_label.pack()
+
+        # Label to display image after the image is selected from the file window
+        raw_image_label = Label(encode_window, text="Select Raw Image", height=20, width=50, relief="solid",
+                                bg="#FFFFFF")
+        # Placing the label on the window statically
+        raw_image_label.place(x=20, y=20)
+
+        # Label to indicate text area to enter the text to be encoded
+        text_to_encode_label = Label(encode_window, text="Text to Encode")
+        text_to_encode_label.config(font=("Open Sans", 12))
+        text_to_encode_label.place(x=400, y=20)
+
+        # Textarea to enter the text to be encoded
+        text_to_encode = Text(encode_window, height=7, width=34)
+        text_to_encode.config(relief="solid", font=("Open Sans", 15))
+        text_to_encode.place(x=400, y=51)
+
+        # Label to indicate the text field to enter the password for encoding
+        pass_to_encode_label = Label(encode_window, text="Password")
+        pass_to_encode_label.config(font=("Open Sans", 12))
+        pass_to_encode_label.place(x=400, y=262)
+
+        # Text field to enter the password for encoding
+        pass_to_encode = Entry(encode_window, width=34)
+        pass_to_encode.config(relief="solid", font=("Open Sans", 15), show="*")
+        pass_to_encode.place(x=400, y=293)
+
+        # Button to browse for raw image to encode the text inside it
+        # Calls a function browse_image() with the label to display the image as argument
+        browse_image_btn = Button(encode_window, text="Browse Raw Image", width=29,
+                                  command=lambda: browse_image(raw_image_label))
+        browse_image_btn.config(font=("Open Sans", 15), bg="#36923B", fg="white", borderwidth=0)
+        browse_image_btn.place(x=20, y=350)
+
+        # Button to encode the text inside the image and validate password field
+        # Calls a function on separate encode class
+        encode_image_btn = Button(encode_window, text="Encode", width=15)
+        encode_image_btn.config(font=("Open Sans", 15), bg="#503066", fg="white", borderwidth=0)
+        encode_image_btn.place(x=592, y=420)
+
         # setting the value of global tracking variable to true
         encode_opened = True
         # assigning a handler to define the actions when the window is tried to close
@@ -60,42 +99,80 @@ def open_decode_window():
         decode_window.geometry('800x500')
         # Setting the resizable property of the decode window to false
         decode_window.resizable(False, False)
+        # Setting the window as child of parent so that it appears at front
+        decode_window.transient(window)
         # Setting the decode window to be DPI aware for different screens
         # windll is Windows OS specific, so must be commented before running on other environments
         windll.shcore.SetProcessDpiAwareness(1)
 
         # Label to display image after the image is selected from the file window
-        stego_image_label = Label(decode_window, text="Select Stego Image", height=20, width=50, relief="solid", bg="#FFFFFF")
+        stego_image_label = Label(decode_window, text="Select Stego Image", height=20, width=50, relief="solid",
+                                  bg="#FFFFFF")
         # Placing the label on the window statically
         stego_image_label.place(x=20, y=20)
 
+        # Label to indicate textarea to display decoded text
         text_to_decode_label = Label(decode_window, text="Decoded Text")
         text_to_decode_label.config(font=("Open Sans", 12))
         text_to_decode_label.place(x=400, y=20)
 
+        # Textarea to display decoded text
         text_to_decode = Text(decode_window, height=7, width=34)
         text_to_decode.config(relief="solid", font=("Open Sans", 15), state=DISABLED)
         text_to_decode.place(x=400, y=51)
 
+        # Label to indicate password field to decode the image
         pass_to_decode_label = Label(decode_window, text="Password")
         pass_to_decode_label.config(font=("Open Sans", 12))
         pass_to_decode_label.place(x=400, y=262)
 
+        # Text field to enter password to decode the image
         pass_to_decode = Entry(decode_window, width=34)
         pass_to_decode.config(relief="solid", font=("Open Sans", 15), show="*")
         pass_to_decode.place(x=400, y=293)
 
-        browse_stego_btn = Button(decode_window, text="Browse Stego Image", width=29)
+        # Button to browse for encoded image or stego image to decode
+        # Calls a function browse_image() with the label to display the image as argument
+        browse_stego_btn = Button(decode_window, text="Browse Stego Image", width=29,
+                                  command=lambda: browse_image(stego_image_label))
         browse_stego_btn.config(font=("Open Sans", 15), bg="#503066", fg="white", borderwidth=0)
         browse_stego_btn.place(x=20, y=350)
 
+        # Button to decode the image and validate password field
+        # Calls a function on separate decode class
         decode_stego_btn = Button(decode_window, text="Decode", width=15)
         decode_stego_btn.config(font=("Open Sans", 15), bg="#36923B", fg="white", borderwidth=0)
         decode_stego_btn.place(x=592, y=420)
 
+        # Setting the status of decode_window as true to denote that decode window is open
         decode_opened = True
         # assigning a handler to define the actions when the window is tried to close
         decode_window.protocol("WM_DELETE_WINDOW", lambda: close_decode_window(decode_window))
+
+
+##########################################################
+# COMMON COMPONENTS
+##########################################################
+# function to open file window to browse image to encode and decode
+def browse_image(image_frame):
+    # Opening the file dialog to allwo user to choose iamge files
+    file_name = filedialog.askopenfilename(title="Choose an Image",
+                                           filetype=(("Image Files", "*.png"), ("Image Files", "*.jpg")))
+    # Getting the path of the selected image
+    selected_image = Image.open(file_name)
+    # Setting the maximum width of the image to display it on the window
+    max_width = 350
+    # Calculating the aspect ratio for proportional image resizing
+    aspect_ratio = max_width / float(selected_image.size[0])
+    # Calculating the proportional height for the defined width
+    max_height = int((float(selected_image.size[1]) * float(aspect_ratio)))
+    # Resizing the image with calculated height and width
+    selected_image = selected_image.resize((max_width, max_height), Image.ANTIALIAS)
+    # converting the selected image format to tkinter compatible image
+    selected_image = ImageTk.PhotoImage(selected_image)
+    # Setting the label configurations to display the image on the label
+    image_frame.config(image=selected_image, height=304, width=354)
+    image_frame.image = selected_image
 
 
 ##########################################################
